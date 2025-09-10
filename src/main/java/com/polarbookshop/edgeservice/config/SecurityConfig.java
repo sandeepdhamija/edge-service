2d6a4f7812b2638378d13754c84f00af395775aa
dhami <dhamija.sandeep.cse@gmail.com>
2025-09-10 13:39:51 -0400

    added oidc auth using keycloak

---
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.polarbookshop.edgeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+      .build();
+    }
+}
